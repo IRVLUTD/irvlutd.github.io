d2360fa4cce072da56f3b2628787aafe35c82565
Jishnu P <jishnu.jayakumar182@gmail.com>
2023-06-28 14:09:08 -0500

    beautify (#32)

--- SceneReplica/assets/js/leaderboard-info.js
+++ SceneReplica/assets/js/leaderboard-info.js
@@ -17,28 +17,28 @@ const leaderboard_entries = [
 const references = {
     "1": `X. Deng, A. Mousavian, Y. Xiang, F. Xia, T. Bretl, and D. Fox. Poserbpf: A rao–blackwellized
     particle filter for 6-d object pose tracking. IEEE Transactions on Robotics, 37(5):1328–1342,
-    2021. <a target="_blank" href="https://github.com/NVlabs/PoseRBPF">Official Code</a>, <a target="_blank" href="https://github.com/IRVLUTD/posecnn-pytorch">SceneReplica Version</a>`,
+    2021. <b>[</b> <a target="_blank" href="https://github.com/NVlabs/PoseRBPF">Official Code</a> | <a target="_blank" href="https://github.com/IRVLUTD/posecnn-pytorch">SceneReplica Version</a> <b>]</b>`,
     "2": `A. T. Miller and P. K. Allen. Graspit! a versatile simulator for robotic grasping. IEEE Robotics
-    & Automation Magazine, 11(4):110–122, 2004. <a target="_blank" href="https://github.com/graspit-simulator">Official Code</a>`,
+    & Automation Magazine, 11(4):110–122, 2004. <b>[</b> <a target="_blank" href="https://github.com/graspit-simulator">Official Code</a> <b>]</b>`,
     "3": `I. A. Sucan, M. Moll, and L. E. Kavraki. The open motion planning library. IEEE Robotics &
-    Automation Magazine, 19(4):72–82, 2012. <a target="_blank" href="https://github.com/ompl/ompl">Official Code</a>`,
+    Automation Magazine, 19(4):72–82, 2012. <b>[</b> <a target="_blank" href="https://github.com/ompl/ompl">Official Code</a> <b>]</b>`,
     "4": `Y. Xiang, T. Schmidt, V. Narayanan, and D. Fox. Posecnn: A convolutional neural network for
     6d object pose estimation in cluttered scenes. arXiv preprint arXiv:1711.00199, 2017.
-    <a target="_blank" href="https://github.com/NVlabs/PoseCNN-PyTorch">Official Code</a>, <a target="_blank" href="https://github.com/IRVLUTD/PoseCNN-PyTorch-NV-Release">SceneReplica Version</a>`,
+    <b>[</b> <a target="_blank" href="https://github.com/NVlabs/PoseCNN-PyTorch">Official Code</a> | <a target="_blank" href="https://github.com/IRVLUTD/PoseCNN-PyTorch-NV-Release">SceneReplica Version</a> <b>]</b>`,
     "5": `Y. Xiang, C. Xie, A. Mousavian, and D. Fox. Learning rgb-d feature embeddings for unseen
     object instance segmentation. In Conference on Robot Learning, pages 461–470. PMLR, 2021.
-    <a target="_blank" href="https://github.com/NVlabs/UnseenObjectClustering">Official Code</a>, <a target="_blank" href="https://github.com/IRVLUTD/UnseenObjectClustering">SceneReplica Version</a>`,
+    <b>[</b> <a target="_blank" href="https://github.com/NVlabs/UnseenObjectClustering">Official Code</a> | <a target="_blank" href="https://github.com/IRVLUTD/UnseenObjectClustering">SceneReplica Version</a> <b>]</b>`,
     "6": `A. Mousavian, C. Eppner, and D. Fox. 6-dof graspnet: Variational grasp generation for object
     manipulation. In Proceedings of the IEEE/CVF International Conference on Computer Vision,
     pages 2901–2910, 2019.
-    <a target="_blank" href="https://github.com/NVlabs/6dof-graspnet">Official Code</a>, <a target="_blank" href="https://github.com/IRVLUTD/pytorch_6dof-graspnet">SceneReplica Version</a>`,
+    <b>[</b> <a target="_blank" href="https://github.com/NVlabs/6dof-graspnet">Official Code</a> | <a target="_blank" href="https://github.com/IRVLUTD/pytorch_6dof-graspnet">SceneReplica Version</a> <b>]</b>`,
     "7": `M. Sundermeyer, A. Mousavian, R. Triebel, and D. Fox. Contact-graspnet: Efficient 6-dof
     grasp generation in cluttered scenes. In 2021 IEEE International Conference on Robotics and
     Automation (ICRA), pages 13438–13444. IEEE, 2021.
-    <a target="_blank" href="https://github.com/NVlabs/contact_graspnet">Official Code</a>, <a target="_blank" href="https://github.com/IRVLUTD/contact_graspnet">SceneReplica Version</a>`,
+    <b>[</b> <a target="_blank" href="https://github.com/NVlabs/contact_graspnet">Official Code</a> | <a target="_blank" href="https://github.com/IRVLUTD/contact_graspnet">SceneReplica Version</a> <b>]</b>`,
     "8": `Y. Lu, Y. Chen, N. Ruozzi, and Y. Xiang. Mean shift mask transformer for unseen object
     instance segmentation. arXiv preprint arXiv:2211.11679, 2022.
-    <a target="_blank" href="https://github.com/YoungSean/UnseenObjectsWithMeanShift">Official Code</a>`
+    <b>[</b> <a target="_blank" href="https://github.com/YoungSean/UnseenObjectsWithMeanShift">Official Code</a> <b>]</b>`
 }
 
 const video_links = {
