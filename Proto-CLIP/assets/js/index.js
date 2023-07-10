d786a5524b2982597b1e469c4f771930d1086088
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2023-07-09 20:03:06 -0500

    + dynamic last updated fix

--- Proto-CLIP/assets/js/index.js
+++ Proto-CLIP/assets/js/index.js
@@ -27,11 +27,4 @@ $(document).ready(function() {
       $('#demo-vid-iframe').html(vid_html);
     });
 
-    // Last updated
-    var apiUrl = "https://api.github.com/repos/irvlutd/irvlutd.github.io/commits?sha=fewsol&per_page=1";
-    $.getJSON(apiUrl, function(data) {
-    var commitDate = new Date(data[0].commit.committer.date);
-    var formattedDate = commitDate.toDateString();
-    $('#last-updated').html(formattedDate);
-    });
 })
